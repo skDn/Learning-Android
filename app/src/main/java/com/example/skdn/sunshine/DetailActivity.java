229aa0f5a83797e38ce35451af4fbe6ba074cfe9
skDn <2038011y@student.gla.ac.uk>
2016-01-31 19:13:25 +0200

    Added Settings activity.

--- app/src/main/java/com/example/skdn/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/skdn/sunshine/DetailActivity.java
@@ -59,6 +59,7 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
